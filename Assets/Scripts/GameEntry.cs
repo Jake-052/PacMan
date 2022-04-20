d05687cb41f9b10b6810369e87a9e1c780cb9118
Jake Armstrong <1846113635@qq.com>
2022-04-20 18:12:54 +0800

    更改解析插件

--- Assets/Scripts/GameEntry.cs
+++ Assets/Scripts/GameEntry.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using LitJson;
 
 class GameEntry : MonoBehaviour
 {
@@ -7,22 +6,17 @@ class GameEntry : MonoBehaviour
 
     public void Start()
     {
-        if (mapModel == null)
-        {
-            Debug.Log("框架问题");
-        }
-        else
-        {
-            Debug.Log("框架没问题");
-        }
 
-        for (int i = 0; i < mapModel.mapDataArray.GetLength(0); i++)
-        {
-            for (int j = 0; j < mapModel.mapDataArray.GetLength(1); j++)
-            {
-                Debug.Log("-----"+mapModel.mapDataArray[i][j]);
-            }
-        }
+
+        //Debug.Log("------"+ mapModel.mapDataArray.GetLength(0));
+
+        //for (int i = 0; i < mapModel.mapDataArray.GetLength(0); i++)
+        //{
+        //    for (int j = 0; j < mapModel.mapDataArray.GetLength(1); j++)
+        //    {
+        //        Debug.Log("-----"+mapModel.mapDataArray[i][j]);
+        //    }
+        //}
        
     }
 }
